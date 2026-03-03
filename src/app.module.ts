9e4e802276c1b0ebba22ad7b20fb2ef1a153b186
qasimhamdani <qasim_hamdani26@milton.edu>
2026-03-02 21:09:37 -0500

    tried

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; 
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://qasimhamdani26:apple@cluster0.dvibg4n.mongodb.net/nestjs-demo')],
   controllers: [AppController],
   providers: [AppService],
 })
